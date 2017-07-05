1fc95a75d5c88478f1dcd34b817163263d722314
Your Name <you@example.com>
2017-07-05 16:12:44 +0800

    implement step 5

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -7,4 +7,12 @@ module JobsHelper
       "(Public)"
     end
   end
+
+  def render_job_status(job)
+    if job.is_hidden
+      content_tag(:span, "", :class => "fa fa-lock")
+    else
+      content_tag(:span, "", :class => "fa fa-globe")
+    end
+  end
 end
